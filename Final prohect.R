32cc17ee0cfab886054ac8dc881545ee7704c74a
Bkc0901 <yoake01010314@gmail.com>
2024-12-18 06:26:43 +0000

    1218

--- Final prohect.R
+++ Final prohect.R
@@ -1,29 +1,19 @@
-# Importing the CSV file
-tidy_energy_data <- readr::read_csv("經濟部能源署_電力消費年資料(2018年後).csv")
+# Load required packages
+library(googlesheets4)
 
-# View the first few rows of the data
-print(tidy_energy_data)
-head(tidy_energy_data)
+# Read the Google Sheet
+tidy_water <- read_sheet("https://docs.google.com/spreadsheets/d/1uWDulSn-N-Hc1CEERQ90AKQ3gJRhe1ac3OslTfgPwKk/edit?usp=sharing")
 
-# Select, retain the `日期(年)` column, and rename the others
-`Electricity_Consumption_by_Sector` <- tidy_energy_data %>%
+# Select and rename columns
+tidy_water_quality <- tidy_water %>%
   select(
-    `日期(年)`,
-    `電力消費_能源部門自用` = `電力消費_能源部門自用`,
-    `電力消費_工業部門_合計` = `電力消費_工業部門_合計`,
-    `電力消費_運輸部門_合計` = `電力消費_運輸部門_合計`,
-    `電力消費_服務業部門_合計` = `電力消費_服務業部門_合計`,
-    `電力消費_住宅部門` = `電力消費_住宅部門`
-  ) %>%
-  rename(
-    Year = `日期(年)`,
-    Energy_Self_Use = `電力消費_能源部門自用`,
-    Industrial_Total = `電力消費_工業部門_合計`,
-    Transport_Total = `電力消費_運輸部門_合計`,
-    Services_Total = `電力消費_服務業部門_合計`,
-    Residential = `電力消費_住宅部門`
-  ) %>%
-  as.data.frame()
-
-# Parse the columns as ordered factors except for `Year`
-
+    month = `月份`,
+    id = `序號`,
+    river_name = `河川名稱`,
+    station = `監測站`,
+    water_temp = `水溫數值（℃）`,
+    air_temp = `氣溫數值（℃）`,
+    pH_value = `pH數值`,
+    dissolved_oxygen = `溶氧量數值（mg/l）`,
+    turbidity = `濁度數值（NTU）`
+  )
