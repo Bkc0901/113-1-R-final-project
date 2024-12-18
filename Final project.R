abe70be29b49f09b736f1002fa1caffd71eece9b
Bkc0901 <yoake01010314@gmail.com>
2024-12-18 14:39:39 +0000

    1218

--- Final project.R
+++ Final project.R
@@ -28,6 +28,39 @@ tidy_water_quality <- tidy_water_quality |>
   )
 summary(tidy_water_quality)
 
-unique(tidy_water_quality$river_name)
-unique(tidy_water_quality$station)
+tidy_water_quality <- tidy_water_quality |>
+  mutate(
+    month = case_when(
+      month %in% c(3, 4, 5) ~ "3-5",
+      month %in% c(6, 7, 8) ~ "6-8",
+      month %in% c(9, 10, 11) ~ "9-11",
+      month %in% c(12, 1, 2) ~ "12-1"
+    ),
+    month = factor(month, levels = c("3-5", "6-8", "9-11", "12-1"), ordered = TRUE)
+  )
+
+tidy_water_quality <- tidy_water_quality |>
+  mutate(
+    Season = case_when(
+      month == "3-5" ~ "Spring",
+      month == "6-8" ~ "Summer",
+      month == "9-11" ~ "Autumn",
+      month == "12-1" ~ "Winter"
+    ),
+    Season = factor(Season, levels = c("Spring", "Summer", "Autumn", "Winter"))
+  ) |>
+  relocate(Season, .after = month)
+
+summary(tidy_water_quality)
+
+tidy_water_quality_summary <- tidy_water_quality |>
+  group_by(river_name, station, Season, month) |>
+  summarize(
+    avg_water_temp = round(mean(water_temp, na.rm = TRUE), 1),
+    avg_air_temp = round(mean(air_temp, na.rm = TRUE), 1),
+    avg_pH = round(mean(pH_value, na.rm = TRUE), 1),
+    avg_dissolved_oxygen = round(mean(dissolved_oxygen, na.rm = TRUE), 1),
+    avg_turbidity = round(mean(turbidity, na.rm = TRUE), 1),
+    .groups = "drop"  # Ensures the result is ungrouped after summarizing
+  )
 
